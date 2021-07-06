5084b7657d2bbf412c134e74f0b2088b7efec5e2
rickvillafane <rickvillafane@gmail.com>
2021-07-06 19:11:30 -0300

    Ejercicio 9

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,8 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
-	alert("ok");
+	var sueldo,aumento;
+	sueldo=parseInt(document.getElementById("txtIdSueldo").value);
+	aumento=sueldo*0.1;
+	document.getElementById("txtIdResultado").value=sueldo+aumento;
 }
