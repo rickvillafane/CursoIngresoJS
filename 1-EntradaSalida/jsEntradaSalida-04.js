f05e0ed2a8e98f567e6f510beca211c22240002c
rickvillafane <rickvillafane@gmail.com>
2021-07-05 21:08:58 -0300

    Ejercicio 4

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,10 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	var nombre,mensaje;
+	nombre=prompt("Ingrese su nombre");
+	mensaje="Su nombre es: " + nombre;
+	document.getElementById("txtIdNombre").value=mensaje;
+	
 }
 
