5626c332c175af7dee8933a681b4d6982e5ed475
rickvillafane <rickvillafane@gmail.com>
2021-07-12 22:56:50 -0300

    Tp N5

--- 8-TPs/05-jsAdivinaElNumero1.js
+++ 8-TPs/05-jsAdivinaElNumero1.js
@@ -7,20 +7,51 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
+var numeroSecreto;
 var contadorIntentos;
 
-function comenzar()
-{
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+function comenzar() {
+  //Genero el número RANDOM entre 1 y 100
+  var numeroUsuario;
 
+  contadorIntentos = 1;
+  numeroSecreto = parseInt(Math.random() * 100) + 1;
+  //console.log(numeroSecreto);
+  numeroUsuario = parseInt(document.getElementById("txtIdNumero").value);
+
+  if (numeroSecreto == numeroUsuario) {
+
+    alert("Usted es el Ganador!!! Y en solo " + contadorIntentos + " intentos!");
+  }
+  else if (numeroSecreto > numeroUsuario) {
+
+    alert("Falta...");
+  }
+  else {
+    alert("Se pasó...");
+  }
+
+  document.getElementById("txtIdIntentos").value = contadorIntentos;
 }
 
-function verificar()
-{
-	
-	
+function verificar() {
+
+  var numeroUsuario;
+  numeroUsuario = parseInt(document.getElementById("txtIdNumero").value);
+  document.getElementById("txtIdIntentos").value = contadorIntentos;
+  contadorIntentos = contadorIntentos + 1;
+
+  if (numeroSecreto == numeroUsuario) {
+
+    alert("Usted es el Ganador!!! Y en solo " + contadorIntentos + " intentos!")
+  }
+  else if (numeroSecreto > numeroUsuario) {
+
+    alert("Falta...")
+  }
+  else {
+    alert("Se pasó...")
+  }
+
+  document.getElementById("txtIdIntentos").value = contadorIntentos;
 }
