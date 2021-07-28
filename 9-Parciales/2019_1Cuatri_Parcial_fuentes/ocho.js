effe827062aa304d565aea6c81b93562b7e9fa15
rickvillafane <rickvillafane@gmail.com>
2021-07-27 21:13:51 -0300

    parcial 2019

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,112 @@
-function mostrar()
-{
+/*
+Bienvenidos.
+Realizar el algoritmo que permita iterar el ingreso de dos datos, una letra y un número entre -100 y 100 (validar) 
+hasta que el usuario quiera e informar al terminar el ingreso por document.write:
+a) La cantidad de números pares.
+b) La cantidad de números impares.
+c) La cantidad de ceros.
+d) El promedio de todos los números positivos ingresados.
+e) La suma de todos los números negativos.
+f) El número y la letra del máximo y el mínimo.
+*/
+/*
+AUTOR RICARDO VILLAFAÑE
+EJ 8 PARCIAL 2019
+*/
+
+function mostrar() {
+    var letra;
+    var numero;
+    var banderaPrimero;
+    var seguir;
+    var contadorCeros;
+    var contadorPositivos;
+    var acumuladorPositivos;
+    var promedioPositivos;
+    var acumuladorNegativos;
+    var contadorPares;
+    var contadorImpares;
+    var numeroMaximo;
+    var letraMaximo;
+    var numeroMinimo;
+    var letraMinimo;
+
+    banderaPrimero = true;
+    contadorCeros = 0;
+    contadorPositivos = 0;
+    acumuladorNegativos = 0;
+    contadorPares = 0;
+    contadorImpares = 0;
+    acumuladorPositivos = 0;
+
+
+    do {
+        letra = prompt("Ingrese una letra");
+        numero = prompt("Ingrese un número entre -100 y 100");
+        numero = parseInt(numero);
+        while (numero < -100 || numero > 100) {
+            numero = prompt("Número no Válido. Ingrese nuevamente entre -100 y 100");
+        }
+        if (banderaPrimero == true) {
+            numeroMaximo = numero;
+            numeroMinimo = numero;
+            letraMaximo = letra;
+            letraMinimo = letra;
+            banderaPrimero = false;
+        }
+        else {
+            if (numeroMaximo < numero) {
+                numeroMaximo = numero;
+                letraMaximo = letra;
+            }
+            else {
+                if (numeroMinimo > numero) {
+                    numeroMinimo = numero;
+                    letraMinimo = letra;
+                }
+            }
+        }
+
+        if (numero == 0) {
+            contadorCeros++;
+        }
+        else {
+            if (numero > 0) {
+                acumuladorPositivos = acumuladorPositivos + numero;
+                contadorPositivos++;
+            }
+            else {
+                acumuladorNegativos = acumuladorNegativos + numero;
+
+            }
+        }
+
+        if (numero % 2 == 0) {
+            contadorPares++;
+        }
+        else {
+            contadorImpares++;
+        }
+
+        seguir = prompt("Desea continuar ingresando datos? Si // No");
+        while (seguir != "Si" && seguir != "No") {
+            seguir = prompt("La respuesta debe ser 'Si' o 'No'");
+        }
+
+
+    } while (seguir == "Si");
+
+    promedioPositivos = acumuladorPositivos / contadorPositivos;
+
+    document.write("La cantidad de numeros pares es " + contadorPares + "<br>" +
+        "La cantidad de números impares es " + contadorImpares + "<br>" +
+        "La cantidad de ceros es " + contadorCeros + "<br>" +
+        "El promedio de todos los números positivos ingresados es " + promedioPositivos + "<br>" +
+        "La suma de los números negativos es " + acumuladorNegativos + "<br>" +
+        "El número Máximo es " + numeroMaximo + " y su letra es: " + letraMaximo + "<br>" +
+        "El número Mínimo es " + numeroMinimo + " y su letra es: " + letraMinimo + "<br>")
+
+
+
 
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,81 @@
-function mostrar()
-{
+/*
+Realizar el algoritmo que permita el ingreso por prompt de las alturas en centimetros(validar entre 0 y 250) , 
+el sexo. (validar el sexo “f” o “m”) de 5 jugadores de básquet, informar por alert:
+a) El promedio de las alturas totales.
+b) La altura más baja y el sexo de esa persona.
+c) La cantidad de muheres que su altura supere los 190 centimetros.
+*/
+/*
+AUTOR RICARDO VILLAFAÑE
+EJ 7 PARCIAL 2019
+*/
 
+function mostrar() {
+
+    var altura;
+    var alturaMasBaja;
+    var promedioAlturas;
+    var sexoMasBajo;
+    var acumuladorAltura;
+    var sexo;
+    var mujeresAltas;
+    var contador;
+
+
+    contador = 0;
+    mujeresAltas = 0;
+    acumuladorAltura = 0;
+
+    for (contador; contador < 5; contador++) {
+
+        sexo = prompt("Ingrese el Sexo del jugador de la siguiente manera; 'M' 'F'");
+
+        while (sexo != "M" && sexo != "F") {
+            sexo=prompt("Dato no válido, 'M' para Masculino, 'F' para Femenino");
+        }
+        altura = prompt("Ingrese la altura del jugador o jugadora en centímetros");
+        altura = parseInt(altura);
+
+        while (altura < 0 || altura > 250) {
+            altura=prompt("Dato no válido, la altura debe estar entre 0cm y 250 cm");
+        }
+
+        if (altura >= 190 && sexo == "F") {
+            mujeresAltas++;
+        }
+
+        if (contador == 0) {
+            alturaMasBaja = altura;
+            sexoMasBajo = sexo;
+        }
+        else {
+            if (altura < alturaMasBaja) {
+                alturaMasBaja = altura;
+                sexoMasBajo = sexo;
+            }
+        }
+
+        acumuladorAltura = acumuladorAltura + altura;
+
+    }
+
+    
+    promedioAlturas = acumuladorAltura / 5;
+
+    if (sexo == "F") {
+        sexo = "Femenino";
+    }
+    else {
+        sexo = "Masculino";
+    }
+
+    alert("El promedio de las alturas es " + promedioAlturas + " cm");
+    alert("La altura más baja es de " + alturaMasBaja + " cm y es de una jugadora de sexo " + sexo);
+
+    if (mujeresAltas == 0) {
+        alert("No hay mujeres que superen los 190cm");
+    }
+    else {
+        alert("Las mujeres que superan los 190cm son: " + mujeresAltas);
+    }
 }
